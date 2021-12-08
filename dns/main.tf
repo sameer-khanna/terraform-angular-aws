e58ec6035909c0676987f4c9c96a038974384938
sameer-khanna <38571919+sameer-khanna@users.noreply.github.com>
2021-12-08 18:35:21 +0530

    Adding ignore changes lifecycle to R53 record

--- dns/main.tf
+++ dns/main.tf
@@ -7,6 +7,9 @@ resource "aws_route53_record" "sampleproject" {
   zone_id = data.aws_route53_zone.primary-hosted-zone.zone_id
   name    = "sampleproject-${substr(uuid(), 0, 4)}.${data.aws_route53_zone.primary-hosted-zone.name}"
   type    = "A"
+  lifecycle {
+    ignore_changes = [name]
+  }
   alias {
     name                   = var.alb_dns_name
     zone_id                = var.alb_dns_zone_id
