060123f2fb98230f73682ed5ad310c8833babdd5
sameer-khanna <38571919+sameer-khanna@users.noreply.github.com>
2022-01-08 19:21:07 +0530

    Formatting HCL

--- terraform/networking/main.tf
+++ terraform/networking/main.tf
@@ -138,10 +138,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_vpc_endpoint" "interface_endpoints" {
-  for_each            = var.vpc_endpoints
-  vpc_id              = aws_vpc.tf-project-vpc.id
-  service_name        = each.value.service_name
-  vpc_endpoint_type   = each.value.vpc_endpoint_type
+  for_each          = var.vpc_endpoints
+  vpc_id            = aws_vpc.tf-project-vpc.id
+  service_name      = each.value.service_name
+  vpc_endpoint_type = each.value.vpc_endpoint_type
   # Being cost conscious using only 1 Systems Manager interface endpoint since this is a test project. In PROD we should provision multiple endpoints across AZs for HA. 
   # subnet_ids          = data.aws_subnets.app_subnets.ids
   subnet_ids          = [data.aws_subnets.app_subnets.ids[0]]
